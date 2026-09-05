20af7f4e20e5b578cdd7161cdcd297082d1201f3
richtman10-hue <richtman10@gmail.com>
2026-09-05 11:36:52 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-const CACHE='albania-2026-final-v6';
+const CACHE='albania-2026-v28';
 const ASSETS=[
+  "./robots.txt",
+  "./apple-touch-icon.png",
+  "./icon-512.png",
+  "./icon-192.png",
   "./",
   "./index.html",
   "./viewer.html",
@@ -73,7 +77,7 @@ const ASSETS=[
   "./previews/orit_israel_valbone_two_e_19-21sep/page-1.webp",
   "./previews/valbone_two_e_19-21sep/page-1.webp"
 ];
-self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('install',event=>{event.waitUntil((async()=>{const c=await caches.open(CACHE);await Promise.all(ASSETS.map(async a=>{try{await c.add(a);}catch(e){console.warn('Cache skip',a,e);}}));await self.skipWaiting();})());});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',event=>{
   const req=event.request;
